5f47b3b261cc9408bba21e8d660f7aca8730b1c7
Artsiom Dziaryba <bruhman2404@gmail.com>
2021-04-13 17:49:51 +0300

    ref: product list endpoint path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  product: 'https://a2qj4p57x3.execute-api.eu-west-1.amazonaws.com/dev/products',
+  product: 'https://a2qj4p57x3.execute-api.eu-west-1.amazonaws.com/dev/product',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
